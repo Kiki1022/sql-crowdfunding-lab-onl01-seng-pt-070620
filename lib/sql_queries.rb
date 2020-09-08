566a35b21977452cb86c6bf07775099378b219b8
Kinnari Shah <kinnari1022@yahoo.com>
2020-09-08 16:13:16 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title 
   FROM projects
-  SUM(pledges.amount) FROM pledges"
+  SELECT SUM(pledges.amount) FROM pledges"
   # ON projects.id = pledges.project_id
   # 
   # ORDER projects.title BY title ASC
